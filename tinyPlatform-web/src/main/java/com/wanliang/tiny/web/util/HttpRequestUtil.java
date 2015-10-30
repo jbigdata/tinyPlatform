127191acbe93a84d22172a7bef12a65317deb9e6
wan_liang@126.com <wan_liang@126.com>
2015-10-30 16:50:33 +0800

    commit

---
+++ tinyPlatform-web/src/main/java/com/wanliang/tiny/web/util/HttpRequestUtil.java
@@ -0,0 +1,35 @@
+package com.wanliang.tiny.web.util;
+
+import org.springframework.util.StringUtils;
+
+import javax.servlet.http.HttpServletRequest;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+/**
+ * Created by Administrator on 2015/10/30.
+ */
+public class HttpRequestUtil {
+    public static String getIpAddr(HttpServletRequest request) {
+        String ip = request.getHeader("x-forwarded-for");
+        if (!StringUtils.hasText(ip) || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if (!StringUtils.hasText(ip) || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (!StringUtils.hasText(ip) || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+        }
+
+        if ("0:0:0:0:0:0:0:1".equals(ip)) {
+            try {
+                ip = InetAddress.getLocalHost().getHostAddress();
+            } catch (UnknownHostException e) {
+                ip = "127.0.0.1";
+            }
+        }
+
+        return ip;
+    }
+}
