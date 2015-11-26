692dc1aee122a47be9e1d1113cb92847429b9c7c
wan_liang@126.com <wan_liang@126.com>
2015-11-26 18:04:31 +0800

    commit

--- microPlatform-web-rest/src/main/java/com/wanliang/micro/web/configuration/DubboConfig.java
+++ microPlatform-web-rest/src/main/java/com/wanliang/micro/web/configuration/DubboConfig.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
 
 /**
  * DubboConfig
@@ -16,51 +17,52 @@ import org.springframework.context.annotation.Configuration;
  * @author Jinkai.Ma
  */
 @Configuration
-@ComponentScan(basePackages = {"com.wanliang.micro.service"}, includeFilters = {@ComponentScan.Filter(value = Service.class)})
+@ImportResource("classpath:dubbo-consumer.xml")
+//@ComponentScan(basePackages = {"com.wanliang.micro.service"}, includeFilters = {@ComponentScan.Filter(value = Service.class)})
 public class DubboConfig {
 
-    @Value("${dubbo.registry.address:zookeeper://127.0.0.11:2181}")
-    private   String REGISTRY_ADDRESS ;
-
-    @Value("${dubbo.application.name:micro}")
-    private   String APPLICATION_NAME ;
-
-    @Value("${dubbo.application.owner:wanliang}")
-    private   String APPLICATION_OWNER ;
-
-    @Value("${dubbo.protocol.port:20880}")
-    private   Integer PROTOCOL_PORT;
-
-    @Value("${dubbo.package.base:com.wanliang.micro.service}")
-    private static  String PACKAGE;
-
-    @Bean
-    public static AnnotationBean annotationBean() {
-        AnnotationBean annotationBean = new AnnotationBean();
-        annotationBean.setPackage("com.wanliang.micro.service");
-        return annotationBean;
-    }
-
-    @Bean
-    public ApplicationConfig applicationConfig() {
-        ApplicationConfig applicationConfig = new ApplicationConfig();
-        applicationConfig.setName(APPLICATION_NAME);
-        applicationConfig.setOwner(APPLICATION_OWNER);
-        return applicationConfig;
-    }
-
-    @Bean
-    public RegistryConfig registryConfig() {
-        RegistryConfig registryConfig = new RegistryConfig();
-        registryConfig.setAddress(REGISTRY_ADDRESS);
-
-        return registryConfig;
-    }
-
-    @Bean
-    public ProtocolConfig protocolConfig() {
-        ProtocolConfig protocolConfig = new ProtocolConfig("dubbo");
-        protocolConfig.setPort(PROTOCOL_PORT);
-        return protocolConfig;
-    }
+//    @Value("${dubbo.registry.address:zookeeper://127.0.0.11:2181}")
+//    private   String REGISTRY_ADDRESS ;
+//
+//    @Value("${dubbo.application.name:micro}")
+//    private   String APPLICATION_NAME ;
+//
+//    @Value("${dubbo.application.owner:wanliang}")
+//    private   String APPLICATION_OWNER ;
+//
+//    @Value("${dubbo.protocol.port:20880}")
+//    private   Integer PROTOCOL_PORT;
+//
+//    @Value("${dubbo.package.base:com.wanliang.micro.service}")
+//    private static  String PACKAGE;
+//
+//    @Bean
+//    public static AnnotationBean annotationBean() {
+//        AnnotationBean annotationBean = new AnnotationBean();
+//        annotationBean.setPackage("com.wanliang.micro.service");
+//        return annotationBean;
+//    }
+//
+//    @Bean
+//    public ApplicationConfig applicationConfig() {
+//        ApplicationConfig applicationConfig = new ApplicationConfig();
+//        applicationConfig.setName(APPLICATION_NAME);
+//        applicationConfig.setOwner(APPLICATION_OWNER);
+//        return applicationConfig;
+//    }
+//
+//    @Bean
+//    public RegistryConfig registryConfig() {
+//        RegistryConfig registryConfig = new RegistryConfig();
+//        registryConfig.setAddress(REGISTRY_ADDRESS);
+//
+//        return registryConfig;
+//    }
+//
+//    @Bean
+//    public ProtocolConfig protocolConfig() {
+//        ProtocolConfig protocolConfig = new ProtocolConfig("dubbo");
+//        protocolConfig.setPort(PROTOCOL_PORT);
+//        return protocolConfig;
+//    }
 }
