e7fe8ba9e9e44952eb9b5940795d08d4fa8628dc
wan_liang@126.com <wan_liang@126.com>
2015-12-04 11:06:29 +0800

    update config

--- microPlatform-service/src/main/java/com/wanliang/micro/service/Application.java
+++ microPlatform-service/src/main/java/com/wanliang/micro/service/Application.java
@@ -1,5 +1,6 @@
 package com.wanliang.micro.service;
 
+import com.wanliang.micro.MybatisScan;
 import com.wanliang.micro.configuration.DataSourceConfig;
 import com.wanliang.micro.service.configruation.DubboConfig;
 import com.wanliang.micro.service.configruation.RedisConfiguration;
@@ -7,6 +8,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
@@ -15,38 +17,19 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
-/**
- * Created by Administrator on 2015/10/30.
- */
+@SpringBootApplication
 @Configuration
 @ComponentScan(basePackageClasses = {Application.class})
 @EnableAutoConfiguration
 @Import({RedisConfiguration.class, DataSourceConfig.class, DubboConfig.class})
-public class Application extends SpringBootServletInitializer implements EmbeddedServletContainerCustomizer {
+public class Application {
 
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(Application.class);
-    }
+    private static final Logger logger = LoggerFactory.getLogger(Application.class);
 
     public static void main(String[] args) {
+
         SpringApplication.run(Application.class, args);
+
     }
 
-
-    @Override
-    public void customize(ConfigurableEmbeddedServletContainer container) {
-        container.setPort(8081);
-    }
 }
-//public class Application {
-//
-//    private static final Logger logger = LoggerFactory.getLogger(Application.class);
-//
-//    public static void main(String[] args) {
-//
-//        SpringApplication.run(Application.class, args);
-//
-//    }
-//
-//}
