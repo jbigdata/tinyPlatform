23ec5abf08c5cda57c22b734842b2da8afc25e3a
wanliang <wan_liang@126.com>
2015-12-21 21:17:12 +0800

    add security

--- microPlatform-common/src/main/java/com/wanliang/micro/common/util/validation/CustomerCheck.java
+++ microPlatform-common/src/main/java/com/wanliang/micro/common/util/validation/CustomerCheck.java
@@ -1,19 +1,19 @@
-package com.wanliang.micro.common.util.validation;
-
-import javax.validation.Constraint;
-import javax.validation.Payload;
-import java.lang.annotation.*;
-
-@Documented
-@Constraint(validatedBy = CustomerCheckValidator.class)
-@Target( { ElementType.TYPE })
-@Retention(RetentionPolicy.RUNTIME)
-public @interface CustomerCheck {
-
-	String message() default "";
-
-	Class<?>[] groups() default {};
-
-	Class<? extends Payload>[] payload() default {};
-
-}
+//package com.wanliang.micro.common.util.validation;
+//
+//import javax.validation.Constraint;
+//import javax.validation.Payload;
+//import java.lang.annotation.*;
+//
+//@Documented
+//@Constraint(validatedBy = CustomerCheckValidator.class)
+//@Target( { ElementType.TYPE })
+//@Retention(RetentionPolicy.RUNTIME)
+//public @interface CustomerCheck {
+//
+//	String message() default "";
+//
+//	Class<?>[] groups() default {};
+//
+//	Class<? extends Payload>[] payload() default {};
+//
+//}
