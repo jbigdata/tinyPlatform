1dfc4630224e33fbe8d64c7e8d1ab4a25d054611
wanliang <wan_liang@126.com>
2016-01-20 21:14:58 +0800

    commit

---
+++ microPlatform-web/src/main/java/com/wanliang/micro/web/controller/back/profile/UserController.java
@@ -0,0 +1,72 @@
+//package com.wanliang.micro.web.controller.profile;
+//
+//import com.alibaba.dubbo.config.annotation.Reference;
+//import com.wanliang.micro.service.system.UserService;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.dao.DataIntegrityViolationException;
+//import org.springframework.security.access.prepost.PreAuthorize;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.validation.BindingResult;
+//import org.springframework.web.bind.WebDataBinder;
+//import org.springframework.web.bind.annotation.*;
+//import org.springframework.web.servlet.ModelAndView;
+//
+//import javax.validation.Valid;
+//import java.util.NoSuchElementException;
+//
+//@Controller
+//public class UserController {
+//
+//    private static final Logger LOGGER = LoggerFactory.getLogger(UserController.class);
+//
+//    @Reference
+//    private UserService userService;
+////    @Autowired
+////    public UserController(UserService userService, UserCreateFormValidator userCreateFormValidator) {
+////        this.userService = userService;
+////        this.userCreateFormValidator = userCreateFormValidator;
+////    }
+////
+////    @InitBinder("form")
+////    public void initBinder(WebDataBinder binder) {
+////        binder.addValidators(userCreateFormValidator);
+////    }
+//
+//   // @PreAuthorize("@currentUserServiceImpl.canAccessUser(principal, #id)")
+//    @RequestMapping("/user/{id}")
+//    public ModelAndView getUserPage(@PathVariable String id) {
+//        LOGGER.debug("Getting user page for user={}", id);
+//        return new ModelAndView("system/user/profile", "user", userService.getUserById(id));
+//}
+//
+////    @PreAuthorize("hasAuthority('ADMIN')")
+////    @RequestMapping(value = "/user/create", method = RequestMethod.GET)
+////    public ModelAndView getUserCreatePage() {
+////        LOGGER.debug("Getting user create form");
+////        return new ModelAndView("user_create", "form", new UserCreateForm());
+////    }
+////
+////    @PreAuthorize("hasAuthority('ADMIN')")
+////    @RequestMapping(value = "/user/create", method = RequestMethod.POST)
+////    public String handleUserCreateForm(@Valid @ModelAttribute("form") UserCreateForm form, BindingResult bindingResult) {
+////        LOGGER.debug("Processing user create form={}, bindingResult={}", form, bindingResult);
+////        if (bindingResult.hasErrors()) {
+////            // failed validation
+////            return "user_create";
+////        }
+////        try {
+////            userService.create(form);
+////        } catch (DataIntegrityViolationException e) {
+////            // probably email already exists - very rare case when multiple admins are adding same user
+////            // at the same time and form validation has passed for more than one of them.
+////            LOGGER.warn("Exception occurred when trying to save the user, assuming duplicate email", e);
+////            bindingResult.reject("email.exists", "Email already exists");
+////            return "user_create";
+////        }
+////        // ok, redirect
+////        return "redirect:/users";
+////    }
+//
+//}
